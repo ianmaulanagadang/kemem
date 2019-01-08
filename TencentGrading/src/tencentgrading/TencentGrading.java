cddb5b50f09722386518a100e6fe6ebf83ba6980
ianmaulanagadang <mremgamer@gmail.com>
2019-01-08 11:02:24 +0700

    hhe

---
+++ TencentGrading/src/tencentgrading/TencentGrading.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tencentgrading;
+
+import GUI.TencentGRading;
+/**
+ *
+ * @author Windows10
+ */
+public class TencentGrading {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new TencentGRading().setVisible(true);
+    }
+    
+}
